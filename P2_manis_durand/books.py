d96a54b38fab58ee78cf5768740c0f01e852cc12
duranddurand <duran.m@icloud.com>
2021-06-19 13:43:04 +0200

    Issue: extracting attribute

--- P2_manis_durand/books.py
+++ P2_manis_durand/books.py
@@ -4,5 +4,12 @@ from bs4 import BeautifulSoup
 url = 'http://books.toscrape.com/index.html'
 
 response = requests.get(url)
-
-print (response)
+if response.ok:
+    links = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    lis = soup.find_all('li')
+    for li in lis:
+        a = li.find('a')
+        link = a['href']
+        links.append(link)
+    print(links)
